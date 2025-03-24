3f38beb4efcd3d4270572312161f2782d59aa5f5
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2025-03-24 20:37:55 -0300

    feat(manager):  Added PivotBase and rearrange the triangles of the piramid.

--- Assets/manager.cs
+++ Assets/manager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,8 +8,13 @@ public class manager : MonoBehaviour {
     public GameObject[] vetGameObj = new GameObject[24];
     GameObject pai;
     Vector3 m_Center;
-	// Use this for initialization
-	void Start () {
+
+    public Transform verticeSuperior;
+    public Transform verticeA, verticeB, verticeC;
+    public Transform tetraedro1, tetraedro2, tetraedro3;
+
+    // Use this for initialization
+    void Start () {
 		for(int i=0; i < 24; i++)
         {
             if(i == 0)
@@ -20,45 +25,80 @@ public class manager : MonoBehaviour {
                 vetGameObj[i]= Instantiate(tetrahedron, new Vector3(vetGameObj[i-1].transform.position.x + 1, 0, 0), vetGameObj[i - 1].transform.rotation);
             //i-1 posicao anterior
         }
-        
+
+
         //Parede Rosa
-        vetGameObj[3].transform.position = new Vector3(0.5f, 0.86603f, 0.28868f);
-        vetGameObj[4].transform.position = new Vector3(1.5f, 0.86603f, 0.28868f);
-        vetGameObj[5].transform.position = new Vector3(1.0f, 1.73206f, 0.57736f);
-        vetGameObj[6].transform.position = new Vector3(2.5f, 0.86565f, 0.2836585f);
-        vetGameObj[6].transform.Rotate(36.396f, 0f, -180f);
-        vetGameObj[7].transform.position = new Vector3(1.5f, 0.86565f, 0.2836585f);
-        vetGameObj[7].transform.Rotate(36.396f, 0f, -180f);
-        vetGameObj[8].transform.position = new Vector3(2f, 1.738f, 0.59f);
-        vetGameObj[8].transform.Rotate(37.293f, 0f, -180f);
-        
-        // Parede Amarela
-        vetGameObj[9].transform.position = new Vector3(1.496f, 0f, 0.862f);
-        vetGameObj[10].transform.position = new Vector3(1f, 0f, 1.725f);
-        vetGameObj[11].transform.position = new Vector3(0.994f, 0.866f, 1.15f);
-        vetGameObj[19].transform.position = new Vector3(1.652f, 0.348f, 0.367f);
-        vetGameObj[19].transform.Rotate(197.378f, -55.35602f, -32.92902f);
-        vetGameObj[20].transform.position = new Vector3(1.147f, 0.348f, 1.231f);
-        vetGameObj[20].transform.Rotate(197.378f, -55.35602f, -32.92902f);
-        vetGameObj[21].transform.position = new Vector3(1.152f, 1.21f, 0.656f);
-        vetGameObj[21].transform.Rotate(197.378f, -55.35602f, -32.92902f);
+        vetGameObj[0].transform.position = new Vector3(0f, 0f, 0f);
+        vetGameObj[1].transform.position = new Vector3(1f, 0f, 0f);
+        vetGameObj[2].transform.position = new Vector3(2f, 0f, 0f);
+        vetGameObj[3].transform.position = new Vector3(0.5f, 0.865f, 0.289f);
+        vetGameObj[4].transform.position = new Vector3(1.5f, 0.865f, 0.289f);
+        vetGameObj[5].transform.position = new Vector3(1f, 1.73f, 0.578f);
+        vetGameObj[13].transform.position = new Vector3(1.5f, 0.865f, 0.289f);
+        vetGameObj[13].transform.Rotate(37f, 0f, 180f);
+        vetGameObj[14].transform.position = new Vector3(2.5f, 0.865f, 0.289f);
+        vetGameObj[14].transform.Rotate(37f, 0f, 180f);
+        vetGameObj[15].transform.position = new Vector3(2f, 1.73f, 0.578f);
+        vetGameObj[15].transform.Rotate(37f, 0f, 180f);
+
+        //Parede Amarela
+        vetGameObj[6].transform.position = new Vector3(1.5f, 0f, 0.865f);
+        vetGameObj[7].transform.position = new Vector3(1f, 0f, 1.732f);
+        vetGameObj[8].transform.position = new Vector3(1f, 0.865f, 1.155f);
+        vetGameObj[19].transform.position = new Vector3(1.658f, 0.35f, 0.364f);
+        vetGameObj[19].transform.Rotate(-162.396f, -55.452f, -32.63599f);
+        vetGameObj[20].transform.position = new Vector3(1.161f, 0.35f, 1.236f);
+        vetGameObj[20].transform.Rotate(-162.396f, -55.452f, -32.63599f);
+        vetGameObj[21].transform.position = new Vector3(1.147f, 1.22f, 0.651f);
+        vetGameObj[21].transform.Rotate(-162.396f, -55.452f, -32.63599f);
 
         //Parede Vermelha
-        vetGameObj[12].transform.position = new Vector3(0.503f, 0f, 0.866f);
-        vetGameObj[16].transform.position = new Vector3(0.5f,0.861f, 0.287f);
-        vetGameObj[16].transform.Rotate(40f, -38.364f, -45f);
-        vetGameObj[17].transform.position = new Vector3(1f, 0.865f, 1.15f);
-        vetGameObj[17].transform.Rotate(40f, -38.364f, -45f);
-        vetGameObj[18].transform.position = new Vector3(1f, 1.7321f, 0.5775f);
-        vetGameObj[18].transform.Rotate(39.95f, -38.41f, -45f);
+        vetGameObj[9].transform.position = new Vector3(0.5f, 0f, 0.865f);
+        vetGameObj[16].transform.position = new Vector3(0.5f, 0.865f, 0.289f);
+        vetGameObj[16].transform.Rotate(40f, -38f, -45f);
+        vetGameObj[17].transform.position = new Vector3(1f, 0.865f, 1.154f);
+        vetGameObj[17].transform.Rotate(40f, -38f, -45f);
+        vetGameObj[18].transform.position = new Vector3(1f, 1.73f, 0.578f);
+        vetGameObj[18].transform.Rotate(40f, -38f, -45f);
+
+        //Face Azul
+        vetGameObj[10].transform.position = new Vector3(0.5f, 0f, 0.865f);
+        vetGameObj[10].transform.Rotate(0f, 60f, 0f);
+        vetGameObj[11].transform.position = new Vector3(1.5f, 0f, 0.865f);
+        vetGameObj[11].transform.Rotate(0f, 60f, 0f);
+        vetGameObj[12].transform.position = new Vector3(1f, 0f, 1.732f);
+        vetGameObj[12].transform.Rotate(0f, 60f, 0f);
+
+        GameObject basePivot = new GameObject("RotaçãoBase");
+        basePivot.transform.position = new Vector3(1.5f, 0f, 0.87561f);
+
+        //Parede Rosa
+        vetGameObj[0].transform.SetParent(basePivot.transform);
+        vetGameObj[1].transform.SetParent(basePivot.transform);
+        vetGameObj[2].transform.SetParent(basePivot.transform);
+        vetGameObj[13].transform.SetParent(basePivot.transform);
+        vetGameObj[14].transform.SetParent(basePivot.transform);
 
         //Parede Azul
-        vetGameObj[13].transform.position = new Vector3(2.5f, 0f, 0.866f);
-        vetGameObj[13].transform.Rotate(0f, 180f, 0f);
-        vetGameObj[14].transform.position = new Vector3(1.5f, 0f, 0.866f);
-        vetGameObj[14].transform.Rotate(0f, 180f, 0f);
-        vetGameObj[15].transform.position = new Vector3(2f, 0f, 1.725f);
-        vetGameObj[15].transform.Rotate(0f, 180f, 0f);
+        vetGameObj[10].transform.SetParent(basePivot.transform);
+        vetGameObj[11].transform.SetParent(basePivot.transform);
+        vetGameObj[12].transform.SetParent(basePivot.transform);
+
+        //Parede Vermelha
+        vetGameObj[9].transform.SetParent(basePivot.transform);
+        vetGameObj[16].transform.SetParent(basePivot.transform);
+        vetGameObj[17].transform.SetParent(basePivot.transform);
+
+        //Parede Amarela
+        vetGameObj[6].transform.SetParent(basePivot.transform);
+        vetGameObj[7].transform.SetParent(basePivot.transform);
+        vetGameObj[19].transform.SetParent(basePivot.transform);
+        vetGameObj[20].transform.SetParent(basePivot.transform);
+
+        //O q isso que isso faz?
+        Vector3 eixoVertical = (verticeSuperior.position - ((verticeA.position + verticeB.position + verticeC.position) / 3f)).normalized;
+
+        basePivot.transform.Rotate(eixoVertical, 120f, Space.World);
 
 
 
